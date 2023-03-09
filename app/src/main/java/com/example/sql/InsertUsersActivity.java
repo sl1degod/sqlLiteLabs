e3e95ace2c6a5cdeb258d511a72e45a6ce61685c
sl1degod <sl1degod86@gmail.com>
2023-03-10 00:12:26 +0300

    voda

--- app/src/main/java/com/example/sql/InsertUsersActivity.java
+++ app/src/main/java/com/example/sql/InsertUsersActivity.java
@@ -10,5 +10,6 @@ public class InsertUsersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert_users);
+//        voda
     }
 }
