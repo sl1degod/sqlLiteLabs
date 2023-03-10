fd3e0e05cb70cc572a4aeaf7725ff0487a6b1ac4
sl1degod <sl1degod86@gmail.com>
2023-03-10 18:57:54 +0300

    start make profile

--- app/src/main/java/com/example/sql/ProfileUserActivity.java
+++ app/src/main/java/com/example/sql/ProfileUserActivity.java
@@ -3,12 +3,19 @@ package com.example.sql;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ProfileUserActivity extends AppCompatActivity {
+
+    TextView login, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_user);
+
+        login = findViewById(R.id.login);
+        password = findViewById(R.id.password);
+
     }
 }
