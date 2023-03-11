381ee8c4e6d11701c19e85479220835b2ff996bf
stud <stud@almetpt.local>
2023-03-11 09:53:07 +0300

    strelka huinya zaebala

--- app/src/main/java/com/example/sql/ProfileUserActivity.java
+++ app/src/main/java/com/example/sql/ProfileUserActivity.java
@@ -1,10 +1,16 @@
 package com.example.sql;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.sql.Network.DataBaseHelper;
 
@@ -12,17 +18,27 @@ public class ProfileUserActivity extends AppCompatActivity {
 
     TextView login, password;
 
+    Toolbar toolbar;
+
     DataBaseHelper db = new DataBaseHelper(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_user);
+        toolbar = findViewById(R.id.toolBar_profile);
+        setSupportActionBar(toolbar);
 
         login = findViewById(R.id.login);
         password = findViewById(R.id.password);
 
+
+
+
+
         setData();
+
+
 
     }
 
@@ -36,5 +52,24 @@ public class ProfileUserActivity extends AppCompatActivity {
             login.setText(cursor.getString(0));
             password.setText(cursor.getString(1));
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_profile, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.arrowBack:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
